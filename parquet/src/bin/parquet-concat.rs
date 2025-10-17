02501bc27446930ea617ae5b0bf1e091d6429b63
Torgeir Børresen <tb@starkad.no>
2025-10-17 07:29:46 +0200

    parquet-concat: handle large number of files.
    This is done by iterating over the file set.
    We check that the schemas agree before concatenating.

--- parquet/src/bin/parquet-concat.rs
+++ parquet/src/bin/parquet-concat.rs
@@ -72,60 +72,67 @@ impl Args {
 
         let output = File::create(&self.output)?;
 
-        let inputs = self
-            .input
-            .iter()
-            .map(|x| {
-                let reader = File::open(x)?;
-                // Enable reading page indexes if present
-                let metadata = ParquetMetaDataReader::new()
-                    .with_page_index_policy(PageIndexPolicy::Optional)
-                    .parse_and_finish(&reader)?;
-                Ok((reader, metadata))
-            })
-            .collect::<Result<Vec<_>>>()?;
+        let schema = {
+            let inputs = self
+                .input
+                .iter()
+                .map(|x| {
+                    let reader = File::open(x)?;
+                    let metadata = ParquetMetaDataReader::new()
+                        .with_page_index_policy(PageIndexPolicy::Optional)
+                        .parse_and_finish(&reader)?;
+                    Ok((reader, metadata))
+                })
+                .collect::<Result<Vec<_>>>()?;
 
-        let expected = inputs[0].1.file_metadata().schema();
-        for (_, metadata) in inputs.iter().skip(1) {
-            let actual = metadata.file_metadata().schema();
-            if expected != actual {
-                return Err(ParquetError::General(format!(
-                    "inputs must have the same schema, {expected:#?} vs {actual:#?}"
-                )));
+            let expected = inputs[0].1.file_metadata().schema();
+            for metadata in inputs.iter().skip(1) {
+                let actual = metadata.1.file_metadata().schema();
+                if expected != actual {
+                    return Err(ParquetError::General(format!(
+                        "inputs must have the same schema, {expected:#?} vs {actual:#?}"
+                    )));
+                }
             }
-        }
 
+            inputs[0].1.file_metadata().schema_descr().root_schema_ptr()
+        };
         let props = Arc::new(WriterProperties::builder().build());
-        let schema = inputs[0].1.file_metadata().schema_descr().root_schema_ptr();
         let mut writer = SerializedFileWriter::new(output, schema, props)?;
 
-        for (input, metadata) in inputs {
-            let column_indexes = metadata.column_index();
-            let offset_indexes = metadata.offset_index();
+        self.input
+            .iter()
+            .map(|x| {
+                let input = File::open(x)?;
+                let metadata = ParquetMetaDataReader::new().parse_and_finish(&input)?;
+                let column_indexes = metadata.column_index();
+                let offset_indexes = metadata.offset_index();
+                for (rg_idx, rg) in metadata.row_groups().iter().enumerate() {
+                    let rg_column_indexes = column_indexes.and_then(|ci| ci.get(rg_idx));
+                    let rg_offset_indexes = offset_indexes.and_then(|oi| oi.get(rg_idx));
 
-            for (rg_idx, rg) in metadata.row_groups().iter().enumerate() {
-                let rg_column_indexes = column_indexes.and_then(|ci| ci.get(rg_idx));
-                let rg_offset_indexes = offset_indexes.and_then(|oi| oi.get(rg_idx));
-                let mut rg_out = writer.next_row_group()?;
-                for (col_idx, column) in rg.columns().iter().enumerate() {
-                    let bloom_filter = read_bloom_filter(column, &input);
-                    let column_index = rg_column_indexes.and_then(|row| row.get(col_idx)).cloned();
-
-                    let offset_index = rg_offset_indexes.and_then(|row| row.get(col_idx)).cloned();
-
-                    let result = ColumnCloseResult {
-                        bytes_written: column.compressed_size() as _,
-                        rows_written: rg.num_rows() as _,
-                        metadata: column.clone(),
-                        bloom_filter,
-                        column_index,
-                        offset_index,
-                    };
-                    rg_out.append_column(&input, result)?;
+                    let mut rg_out = writer.next_row_group()?;
+                    for (col_idx, column) in rg.columns().iter().enumerate() {
+                        let bloom_filter = read_bloom_filter(column, &input);
+                        let column_index =
+                            rg_column_indexes.and_then(|row| row.get(col_idx)).cloned();
+                        let offset_index =
+                            rg_offset_indexes.and_then(|row| row.get(col_idx)).cloned();
+                        let result = ColumnCloseResult {
+                            bytes_written: column.compressed_size() as _,
+                            rows_written: rg.num_rows() as _,
+                            metadata: column.clone(),
+                            bloom_filter,
+                            column_index,
+                            offset_index,
+                        };
+                        rg_out.append_column(&input, result)?;
+                    }
+                    rg_out.close()?;
                 }
-                rg_out.close()?;
-            }
-        }
+                Ok(())
+            })
+            .collect::<Result<Vec<_>>>()?;
 
         writer.close()?;
 
