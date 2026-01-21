0e8f484768fafb01195e54bf9028349ab6110746
Torgeir Børresen <tb@starkad.no>
2026-01-21 22:18:31 +0100

    parquet-concat: Include tests.

--- parquet/src/bin/parquet-concat.rs
+++ parquet/src/bin/parquet-concat.rs
@@ -94,7 +94,7 @@ impl Args {
             }
         }
 
-        // Copy to output file
+        // Given equal schemas, Copy to output file
         let output = File::create(&self.output)?;
         let props = Arc::new(WriterProperties::builder().build());
         let mut writer = SerializedFileWriter::new(output, expected, props)?;
@@ -103,7 +103,9 @@ impl Args {
             .iter()
             .map(|x| {
                 let input = File::open(x)?;
-                let metadata = ParquetMetaDataReader::new().parse_and_finish(&input)?;
+                let metadata = ParquetMetaDataReader::new()
+                    .with_page_index_policy(PageIndexPolicy::Optional)
+                    .parse_and_finish(&input)?;
                 let column_indexes = metadata.column_index();
                 let offset_indexes = metadata.offset_index();
                 for (rg_idx, rg) in metadata.row_groups().iter().enumerate() {
@@ -141,4 +143,287 @@ impl Args {
 
 fn main() -> Result<()> {
     Args::parse().run()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use arrow::error::ArrowError;
+    use arrow_array::{Int32Array, Int64Array, LargeStringArray, RecordBatch};
+    use arrow_schema::{DataType, Field, Schema};
+    use parquet::arrow::ArrowWriter;
+    use parquet::arrow::arrow_reader::ParquetRecordBatchReader;
+    use std::clone::Clone;
+    use std::sync::Arc;
+    use tempfile::TempDir;
+
+    type ArrowResult<T> = Result<T, ArrowError>;
+
+    #[test]
+    #[should_panic]
+    fn test_0_cli_arguments_is_invalid() {
+        Args::try_parse_from(vec![""]).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_single_cli_argument_is_invalid() {
+        Args::try_parse_from(vec!["parquet-concat"]).unwrap();
+    }
+
+    #[test]
+    fn test_need_at_least_one_input_file() -> Result<()> {
+        let tmpf = TempDir::new().unwrap();
+        let out_name = tmpf
+            .path()
+            .join("out.parquet")
+            .into_os_string()
+            .into_string()
+            .expect("retrieved os_string as string");
+        let args =
+            Args::try_parse_from(vec!["parquet-concat", &out_name]).expect("parse arguments");
+
+        let exp_err_msg = "Must provide at least one input file";
+
+        match args.run() {
+            Err(ParquetError::General(msg)) if msg == exp_err_msg => {}
+            _ => panic!("expected ParquetError::General(Must provide at least one input file)"),
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_observes_if_input_files_do_not_exist_produces_error_of_type_external() -> Result<()> {
+        let tmpf = TempDir::new().expect("create TempDir");
+        let out_name = tmpf.path().join("out.parquet");
+
+        let in1_name = tmpf.path().join("in1.parquet");
+        let in2_name = tmpf.path().join("in2.parquet");
+        let in3_name = tmpf.path().join("in3.parquet");
+
+        let inputs = vec![
+            in1_name.to_str().unwrap().to_string(),
+            in2_name.to_str().unwrap().to_string(),
+            in3_name.to_str().unwrap().to_string(),
+        ];
+
+        let args = Args {
+            output: out_name.to_str().unwrap().to_string(),
+            input: inputs,
+        };
+
+        let o = args.run();
+
+        match o {
+            Err(ParquetError::External(_)) => (),
+            _ => panic!("expected External ParquetError"),
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_concatenate_3_files() -> ArrowResult<()> {
+        let num_files = 3;
+
+        let tmpf = TempDir::new().unwrap();
+        let res = run_concat_n_files(&tmpf, num_files, DatasetOne::to_rcb);
+
+        match res {
+            Ok(()) => (),
+            Err(e) => panic!("expected Ok(()), got {}", e),
+        }
+
+        let reader = File::open(tmpf.path().join("out.parquet"))?;
+        let mut expected = Vec::<DatasetOne>::with_capacity(3 * 16);
+        for _ind in 0..num_files {
+            expected.extend(DatasetOne::get());
+        }
+
+        let batch_reader: ParquetRecordBatchReader =
+            ParquetRecordBatchReader::try_new(reader, 1000000).expect("reads out.parquet");
+
+        let batches: ArrowResult<Vec<RecordBatch>> =
+            batch_reader.into_iter().collect::<ArrowResult<Vec<_>>>();
+
+        batches.map(|_t| ())
+    }
+
+    #[test]
+    fn test_concatenate_3000_files() -> Result<()> {
+        let num_files = 3000;
+        let tmpf = TempDir::new().unwrap();
+        run_concat_n_files(&tmpf, num_files, DatasetOne::to_rcb)
+    }
+
+    #[test]
+    fn test_concatenate_2_files_different_schemas() -> Result<()> {
+        let tmpf = TempDir::new().unwrap();
+        let out_name = tmpf.path().join("out.parquet");
+        let inputs: Vec<String> = vec![
+            tmpf.path().join("in1.parquet").display().to_string(),
+            tmpf.path().join("in2.parquet").display().to_string(),
+        ];
+
+        DatasetOne::to_rcb(&inputs[0]);
+        DatasetTwo::to_rcb(&inputs[1]);
+
+        let args = Args {
+            output: out_name.to_str().unwrap().to_string(),
+            input: inputs,
+        };
+
+        match args.run() {
+            Err(ParquetError::General(msg)) => {
+                assert!(msg.starts_with("inputs must have the same schema"));
+            }
+            _ => panic!("expected ParquetError::General"),
+        };
+
+        Ok(())
+    }
+
+    /// First dataset has differing `length` column in i32
+    #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+    struct DatasetOne {
+        pub length: i32,
+        pub annotation: String,
+    }
+
+    impl DatasetOne {
+        pub fn get_schema() -> Arc<Schema> {
+            Arc::new(Schema::new(vec![
+                Field::new("total_length", DataType::Int32, false),
+                Field::new("animal", DataType::LargeUtf8, false),
+            ]))
+        }
+
+        pub fn get_recordbatch() -> RecordBatch {
+            let schema = Self::get_schema();
+
+            let col2_data = Arc::new(LargeStringArray::from_iter_values([
+                "mouse", "cat", "horse", "mouse", "cat", "horse", "mouse", "cat", "mouse", "cat",
+                "horse", "mouse", "cat", "horse", "mouse", "cat",
+            ])) as Arc<dyn arrow_array::Array>;
+            let col1_data = Arc::new(Int32Array::from_iter_values(0..(col2_data.len() as i32)))
+                as Arc<dyn arrow_array::Array>;
+
+            assert_eq!(
+                // defensive check
+                col1_data.len(),
+                col2_data.len(),
+                "columns are of equal length"
+            );
+
+            RecordBatch::try_new(schema.clone(), vec![col1_data, col2_data]).unwrap()
+        }
+
+        fn to_rcb(path: &str) {
+            let record_batch = Self::get_recordbatch();
+            let mut buffer = File::create(path).expect("create file at {path}");
+            let mut writer = ArrowWriter::try_new(&mut buffer, record_batch.schema(), None)
+                .expect("opened file writer");
+            writer.write(&record_batch).expect("wrote to file");
+
+            writer.close().expect("closed writer");
+        }
+
+        fn from_recordbatch(record_batch: &RecordBatch) -> Vec<DatasetOne> {
+            let column1 = record_batch
+                .column(0)
+                .as_any()
+                .downcast_ref::<Int32Array>()
+                .expect("failed to downcast");
+            let column2 = record_batch
+                .column(1)
+                .as_any()
+                .downcast_ref::<LargeStringArray>()
+                .expect("failed to downcast");
+
+            let mut ds: Vec<DatasetOne> = vec![];
+            for (ii, ss) in column1.iter().zip(column2.iter()) {
+                ds.push(DatasetOne {
+                    length: ii.expect("to be number"),
+                    annotation: ss.expect("to be string").to_string(),
+                });
+            }
+            ds
+        }
+
+        fn get() -> Vec<DatasetOne> {
+            let rb = Self::get_recordbatch();
+            Self::from_recordbatch(&rb)
+        }
+    }
+
+    /// First dataset has differing `length` column in i64
+    #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+    struct DatasetTwo {
+        pub length: i64,
+        pub annotation: String,
+    }
+
+    impl DatasetTwo {
+        pub fn get_schema() -> Arc<Schema> {
+            Arc::new(Schema::new(vec![
+                Field::new("length", DataType::Int64, false),
+                Field::new("String", DataType::LargeUtf8, false),
+            ]))
+        }
+
+        pub fn get_recordbatch() -> RecordBatch {
+            let schema = Self::get_schema();
+
+            let col2_data = Arc::new(LargeStringArray::from_iter_values([
+                "mouse", "cat", "horse", "mouse", "cat", "horse", "mouse", "cat", "mouse", "cat",
+                "horse", "mouse", "cat", "horse", "mouse", "cat",
+            ])) as Arc<dyn arrow_array::Array>;
+
+            let col1_data = Arc::new(Int64Array::from_iter_values(0..(col2_data.len() as i64)))
+                as Arc<dyn arrow_array::Array>;
+
+            assert_eq!(
+                // defensive check
+                col1_data.len(),
+                col2_data.len(),
+                "columns are of equal length"
+            );
+
+            RecordBatch::try_new(schema.clone(), vec![col1_data, col2_data])
+                .expect("create recordbatch")
+        }
+
+        fn to_rcb(path: &str) {
+            let record_batch = Self::get_recordbatch();
+            let mut buffer = File::create(path).expect("create file at {path}");
+            let mut writer = ArrowWriter::try_new(&mut buffer, record_batch.schema(), None)
+                .expect("opened file writer");
+            writer.write(&record_batch).expect("wrote to file");
+
+            writer.close().expect("closed writer");
+        }
+    }
+
+    /// Create `num_files` files of type `DatasetOne`
+    /// and then run `parquet-concat` on them
+    fn run_concat_n_files(tmpf: &TempDir, num_files: usize, to_rcb: impl Fn(&str)) -> Result<()> {
+        let out_name = tmpf.path().join("out.parquet");
+
+        let mut inputs = Vec::<String>::with_capacity(num_files);
+        for file_index in 1..=num_files {
+            let in_name = tmpf.path().join(format!("in{file_index}.parquet"));
+            let file_name = in_name.display().to_string();
+            to_rcb(&file_name);
+            inputs.push(file_name);
+        }
+
+        let args = Args {
+            //output: out_name.to_str().unwrap().to_string(),
+            output: out_name.display().to_string(),
+            input: inputs,
+        };
+
+        args.run()
+    }
 }
